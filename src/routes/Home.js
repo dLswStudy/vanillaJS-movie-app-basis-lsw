e58db6ec0044182eb0b528fba830f46a8c43bab6
dLswStudy <devLswStudy@gmail.com>
2024-02-02 13:51:43 +0900

    08~09 컴포넌트 - 상태관리(Store)

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,4 +1,7 @@
 import {Component} from "../core/heropy";
+import TextField from "../components/TextField";
+import Message from "../components/Message";
+import Title from "../components/Title";
 
 export default class Home extends Component {
     constructor() {
@@ -7,6 +10,12 @@ export default class Home extends Component {
     render() {
         this.el.innerHTML = `
             <h1>Home Page!</h1>
+            &nbsp;
         `
+        this.el.append(
+            new TextField().el,
+            new Message().el,
+            new Title().el,
+        )
     }
 }
